abafc5134f78c5ac23c9054af07ccb12bfaed480
Urban20 <173964326+Urban20@users.noreply.github.com>
2026-03-21 21:37:08 -0300

    añado barra de progreso simple

--- src/QubeCore/downloader/goruntinas/goruntinas.go
+++ src/QubeCore/downloader/goruntinas/goruntinas.go
@@ -5,17 +5,22 @@ import (
 	"downloader/red"
 	"fmt"
 	"sync"
+	"sync/atomic"
+
+	"github.com/pterm/pterm"
 )
 
 // este modulo maneja la concurrencia del downloader
 
-func worker(wg *sync.WaitGroup, errores chan string, ch chan data.Task) {
+func worker(wg *sync.WaitGroup, errores chan string, ch chan data.Task, conteo *atomic.Int64, total_carga int) {
 
 	defer wg.Done()
 	for task := range ch {
 		err := red.DownloadFile(task.URL, task.DestPath, task.SHA1)
 
 		//n := done.Add(1) // hace un conteo sincronizado, si lo quiero hacer con i++ simple no sigue sucesion
+		conteo.Add(1)
+		pterm.BgGreen.Printf("\r(+) [%d/%d]", total_carga, conteo.Load())
 
 		if err != nil {
 			errores <- fmt.Sprintf("FALLO [%s]: %v", task.Label, err) // recolecta errores en caso de haberlo
@@ -28,17 +33,18 @@ func worker(wg *sync.WaitGroup, errores chan string, ch chan data.Task) {
 }
 
 // funcion auxiliar para runworkers
-func lanzar_goruntina(wg *sync.WaitGroup, errores chan string, workers int, ch chan data.Task) {
+func lanzar_goruntina(wg *sync.WaitGroup, errores chan string, workers int, ch chan data.Task, conteo *atomic.Int64, total_tareas int) {
 
 	for i := 0; i < workers; i++ {
 		wg.Add(1)
 
-		go worker(wg, errores, ch)
+		go worker(wg, errores, ch, conteo, total_tareas)
 	}
 
 	wg.Wait()
 	close(errores) // espera hasta la ultima goruntina y cierra el canal, en ese orde NO OLVIDAR
 	// quien escribe el canal? las goruntinas , cuando terminan? primero debo esperar (wg.wait) y luego cierro el canal
+	fmt.Print("\n")
 
 	if len(errores) > 0 { //imprime los errores que recolecto
 		fmt.Printf("\n%d error/es:\n", len(errores))
@@ -51,14 +57,17 @@ func lanzar_goruntina(wg *sync.WaitGroup, errores chan string, workers int, ch c
 
 // descarga los archivos con concurrencia para agilizar
 func RunWorkers(tasks []data.Task, workers int) {
-	ch := make(chan data.Task, len(tasks))
-	errores := make(chan string, len(tasks))
+	var total_tareas = len(tasks)
+
+	ch := make(chan data.Task, total_tareas)
+	errores := make(chan string, total_tareas)
 	for _, t := range tasks { // pasa las tareas
 		ch <- t
 	}
 	close(ch)
 
 	var wg = sync.WaitGroup{}
+	var conteo atomic.Int64
 
-	lanzar_goruntina(&wg, errores, workers, ch)
+	lanzar_goruntina(&wg, errores, workers, ch, &conteo, total_tareas)
 }
