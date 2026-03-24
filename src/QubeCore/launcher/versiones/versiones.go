942aa2cd52b7f846a990cf4a6cac8cd638ca8695
Urban20 <173964326+Urban20@users.noreply.github.com>
2026-03-24 11:58:05 -0300

    modifico dir de trabajo del programa

--- src/QubeCore/launcher/versiones/versiones.go
+++ src/QubeCore/launcher/versiones/versiones.go
@@ -15,7 +15,8 @@ import (
 )
 
 var (
-	Exe, _         = os.Getwd()
+	Exe_archivo, _ = os.Executable()
+	Exe            = filepath.Dir(Exe_archivo) //ruta del exe
 	Ruta_minecraft = filepath.Clean(filepath.Join(Exe, ".minecraft"))
 	Ruta_versiones = filepath.Join(Ruta_minecraft, "versions")
 )
